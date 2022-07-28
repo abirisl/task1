4f7d5d35f91f23023b276af1dcf136c96ccead86
abirisl <mhabir979292@gmail.com>
2022-07-28 19:06:53 +0600

    card heading

--- src/component/Home/CardHeading.js
+++ src/component/Home/CardHeading.js
@@ -2,18 +2,24 @@ import React from 'react';
 
 const CardHeading = () => {
     return (
-        <div className='d-flex mx-auto '>
-            <div className=''>
-                <ul className='d-flex list-inline'>
-                    <li>All Posts(32)</li>
-                    <li className='ml-3'>Article</li>
-                    <li>Event</li>
-                    <li>Education</li>
-                    <li>Job</li>
-                </ul>
-            </div>
-            <div>
+        <div className='container mt-3 '>
+            <div className='row'>
+                <div className='col-md-8 d-flex gap-3'>
+                    <p className='border-2 border-bottom border-dark'>All Posts(32)</p>
+                    <p>Article</p>
+                    <p>Event</p>
+                    <p>Education</p>
 
+                </div>
+                <div className='col-md-4'>
+                    <div className='d-flex gap-3 ms-5 '>
+                        <button class="rounded dropdown-toggle border-0" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Write a Post
+                        </button>
+                        <button className='join-button rounded text-white border-0'>Join Group</button>
+                    </div>
+                </div>
+                <hr />
             </div>
         </div>
     );
